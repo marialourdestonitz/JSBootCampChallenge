471f3ea2f54bcc1104cf980b661fd96b7d6eff72
marialourdestonitz <marialourdestonitz@outlook.com>
2022-09-26 15:25:04 +0200

    Initial Commit

---
+++ JSArray/script.js
@@ -0,0 +1,72 @@
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+// const years = new Array[1991, 1992, 2022];
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+
+const maria = ['firstName', 'Tonitz', 2037 - 1989, 'programmer', friends];
+console.log(maria);
+console.log(maria.length);
+
+const calcAge = function (birhtYear) {
+    return 2037 - birhtYear;
+}
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = (calcAge(years[0]));
+const age2 = (calcAge(years[1]));
+const age3 = (calcAge(years[years.length - 1]));
+
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
+
+
+const friends1 = ['Maria', 'Daniel', 'Dahlia'];
+
+//Add Element
+const newLength = friends1.push('Jay');
+console.log(friends1);
+console.log(newLength);
+
+friends1.unshift('Vianeze');
+console.log(friends1);
+
+//Remove Element
+friends1.pop(); //last
+const popped = friends1.pop();
+console.log(popped);
+console.log(friends1);
+
+friends1.shift();
+console.log(friends1);
+
+
+console.log(friends1.indexOf('Daniel'));
+console.log(friends1.indexOf('Oma'));
+
+console.log(friends1.includes('Daniel'));
+console.log(friends1.includes('Oma'));
+
+if (friends1.includes('Daniel')) {
+    console.log('You have friend called Daniel');
+}
+
+//Challenge 
+
+const calcTip = function (bill) {
+    return bill >= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+}
+const bill = [125, 555, 44];
+const tip = [calcTip(bill[0]), calcTip(bill[1]), calcTip(bill[2])];
+const total = [bill[0] + tip[0], bill[1] + tip[1], bill[2] + tip[2]];
+
+console.log(bill, tip, total);
--- JSFunctions/script.js
+++ JSFunctions/script.js
@@ -1,9 +1,90 @@
 'use strict';
-function fruitProcessor(apples, oranges) {
-    console.log(apples, oranges);
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-    return juice;
+// function fruitProcessor(apples, oranges) {
+//     console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+//Function declarations
+
+// function calcAge1(birhtYear) {
+//     return 2037 - birhtYear;
+// }
+
+// const age1 = calcAge1(1991);
+
+// //Functions expression
+// const calcAge2 = function (birhtYear) {
+//     return 2037 - birhtYear;
+// }
+// const age2 = calcAge2(1991);
+
+// console.log(age1, age2);
+
+// //Arrow Function
+// const calcAge3 = birhtYear => 2037 - birhtYear;
+// const age3 = calcAge3(1991);
+// console.log(age3);
+
+// const yearUntilRetirement = (birhtYear, firstName) => {
+//     const age = 2037 - birhtYear;
+//     const retirement = 65 - age;
+//     return `${firstName} retires in ${retirement} years`;
+// }
+
+// console.log(yearUntilRetirement(1991, 'Maria'));
+
+//Functions Calling other Functions
+function cutFruitPieces(fruit) {
+    return fruit * 4;
 }
 
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${apples} piece of apples and ${oranges} pipces of oranges.`;
+    return juice;
+}
+console.log(fruitProcessor(2, 3));
+
+
+//Chalenge No.1
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+
+
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+    if (avgDolphins >= 2 * avgKoalas) {
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas} Koalas)`);
+    }
+    else if (avgKoalas >= 2 * avgDolphins) {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins} Dolphins)`)
+    }
+    else {
+        console.log('No team Wins...')
+    }
+}
+
+checkWinner(scoreDolphins, scoreKoalas);
+
+checkWinner(576, 111);
+
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+
+console.log(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins, scoreKoalas);
+
+
+
